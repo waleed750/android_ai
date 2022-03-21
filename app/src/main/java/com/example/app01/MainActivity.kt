f7a37fc1c14d294bbfd36aa9c4d5866e240f8704
Waled <dedoashraf700@gmail.com>
2022-03-21 17:39:48 +0200

    waleed

---
+++ app/src/main/java/com/example/app01/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.app01
+
+import android.content.Context
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+    fun getMac(context: Context): String {
+        val macAddress =
+            Settings.Secure.getString(this.applicationContext.contentResolver, "android_id")
+        return  macAddress
+    }
+}
